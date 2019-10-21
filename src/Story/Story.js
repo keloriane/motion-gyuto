3f45d9481ffc2deeae808754f4173c0aad16353b
keloriane <kevin.flbt@gmail.com>
2019-10-21 11:12:09 +0200

    paralax scrolling

---
+++ src/Story/Story.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+const Story = () => {
+  return (
+    <div>
+      <div className="background--effect"></div>
+      <div className="side--bar"></div>
+      <div className="nav--story-bar"></div>
+    </div>
+  );
+};
+export default Story;
